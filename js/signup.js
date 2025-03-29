193be4eef8429d942b2b3468e707e50f1ba9ec99
nVarap <nvarap@gmail.com>
2025-03-28 20:20:55 -0700

    Added a lot of new JS functionality, added new pages, got full set up working

---
+++ js/signup.js
@@ -0,0 +1,12 @@
+// Confirm Password Validation
+function validatePasswords(event) {
+    const password = document.getElementById("password").value;
+    const confirmPassword = document.getElementById("confirm-password").value;
+
+    if (password !== confirmPassword) {
+        event.preventDefault(); // Prevent form submission
+        const errorMessage = document.getElementById("error-message");
+        errorMessage.textContent = "Passwords do not match!";
+        errorMessage.style.display = "block";
+    }
+}
---
+++ js/user-home.js
@@ -0,0 +1,7 @@
+function fasterPreview( uploader ) {
+    if ( uploader.files && uploader.files[0] ){
+          $('#profile-pic').attr('src', 
+             window.URL.createObjectURL(uploader.files[0]) );
+    }
+}
+
