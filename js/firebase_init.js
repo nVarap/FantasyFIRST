5358525de63bb62d1c8b4b3b1b42fcc756d71f93
nVarap <nvarap@gmail.com>
2025-04-30 23:16:13 -0700

    Deleting secrets, because that's bad

--- js/firebase_init.js
+++ js/firebase_init.js
@@ -7,13 +7,7 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDy2qLHVfB6nXPhc8DdIyoj7UWNVrSgD-Y",
-  authDomain: "fantasyfirst-9cc36.firebaseapp.com",
-  projectId: "fantasyfirst-9cc36",
-  storageBucket: "fantasyfirst-9cc36.firebasestorage.app",
-  messagingSenderId: "591603968720",
-  appId: "1:591603968720:web:93286e1a7d53fcea9d8cd5",
-  measurementId: "G-HTVHX3EL9H"
+  
 };
 
 // Initialize Firebase
